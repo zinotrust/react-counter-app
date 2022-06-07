4e68bef00bb85025ada630b0b967b221741a856c
ZinoTrust <zinotrust@gmail.com>
2022-06-07 21:49:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
 import "./App.scss";
+import { useState } from "react";
+import Button from "./components/button/Button";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  let color = "#fff";
+  if (count >= 1) {
+    color = "yellow";
+  } else if (count < 0) {
+    color = "red";
+  } else {
+    color = "white";
+  }
+
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <section className="counter-sec --flex-center">
+      <div className="container counter --card --center-all ">
+        <h1 className="--text-light">React Counter App</h1>
+        <p
+          className="count --my2 --text-md --text-light --fw-bold"
+          style={{ color: color }}
+        >
+          {count}
+        </p>
+        <div className="buttons">
+          {/* <button className="--btn --btn-danger" onClick={handleSubtract}>
+            - Subtract
+          </button> */}
+          <Button
+            onClick={handleSubtract}
+            btnClass={"--btn-danger"}
+          >{`- Subtract`}</Button>
+
+          {/* <button className="--btn" onClick={handleReset}>
+            ! Reset
+          </button> */}
+          <Button onClick={handleReset} btnClass={null}>{`! Reset`}</Button>
+
+          {/* <button className="--btn --btn-success" onClick={handleAdd}>
+            + Add
+          </button> */}
+          <Button
+            onClick={handleAdd}
+            btnClass={"--btn-success"}
+          >{`+ Add`}</Button>
+        </div>
+      </div>
+    </section>
   );
 }
 
